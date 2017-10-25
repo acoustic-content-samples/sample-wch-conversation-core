4c780087fd5b5c1ed71c5cb4ef822475ff390509
Sven Sterbling <sterbling@de.ibm.com>
2017-10-25 18:13:31 +0200

    Added newly added plugins to conversation-core. Conversation Service now inits correctly.

--- index.js
+++ index.js
@@ -11,6 +11,50 @@ const architectConfig = require('./lib/conversation-core')();
 resolveConfigAsync(architectConfig, join(__dirname, 'lib'))
 .then(createAppAsync)
 .then(app => {
-  console.log(app);
+
+  let initialContext = {
+    clientType: 'slack',
+    outputlang: '',
+    setoutputlang: true,
+    conversation_id: 'a2a7ea69-1452-4bb6-b1c2-a7506d639cc5',
+    wchid: '62cb7faa-0773-47e5-848e-4e660c4f3e5a',
+    counters: [],
+    gotfunky: false,
+    username: '',
+    chatbotpersona: 'mrwatson',
+    askedContactDetails: false,
+    nodename: 'Response First Welcome',
+    lastIntent: 'welcome'
+  }
+
+  let bot =  {
+    type: 'slack',
+    botkit: {
+      storage: {
+        users: {
+          get: (userId, cb) => {console.log('userId', userId); cb(null, {context: initialContext})},
+          save: (user_data, cb) => {initialContext = user_data.context; cb();}
+        }
+      }
+    }
+  };
+
+  let message = {
+    type: 'message',
+    channel: 'D5BG9R0VD',
+    user: 'W4081DWRF',
+    text: 'Hallo',
+    ts: '1508942774.000657',
+    source_team: 'T02J3DPUE',
+    team: 'T02J3DPUE',
+    event: 'direct_message',
+    match: [ 'Hello', {index: 0}, {input: 'Hello'} ]
+  };
+
+  app.getService('watsonconversation').get('en').interpret(bot, message, () => {
+    console.log('Result ', message.watsonData);
+  });
+
+
 })
 .catch(console.log);
