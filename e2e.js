e37b8bf3f94febc6f0611f34cc21f49b197a7c83
Sven Sterbling <sterbling@de.ibm.com>
2017-11-27 17:55:57 +0100

    Cleaned up index.js. Placed current text into e2e.js.

---
+++ e2e.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const {promisify} = require('util');
+const {join} = require('path');
+const {createApp, resolveConfig} = require('architect');
+const createAppAsync = promisify(createApp);
+const resolveConfigAsync = promisify(resolveConfig);
+
+const architectConfig = require('./lib/conversation-core')();
+
+resolveConfigAsync(architectConfig, join(__dirname))
+.then(createAppAsync)
+.then(app => {
+
+  let initialContext = {
+    clientType: 'slack',
+    outputlang: '',
+    setoutputlang: true,
+    conversation_id: 'a2a7ea69-1452-4bb6-b1c2-a7506d639cc5',
+    wchid: '62cb7faa-0773-47e5-848e-4e660c4f3e5a',
+    counters: [],
+    gotfunky: false,
+    username: '',
+    chatbotpersona: 'mrwatson',
+    askedContactDetails: false,
+    nodename: 'Response First Welcome',
+    lastIntent: 'welcome'
+  }
+
+  let bot =  {
+    type: 'slack',
+    botkit: {
+      storage: {
+        users: {
+          get: (userId, cb) => {console.log('userId', userId); cb(null, {context: initialContext})},
+          save: (user_data, cb) => {initialContext = user_data.context; cb();}
+        }
+      }
+    }
+  };
+
+  let message = {
+    type: 'message',
+    channel: 'D5BG9R0VD',
+    user: 'W4081DWRF',
+    text: 'Hallo',
+    ts: '1508942774.000657',
+    source_team: 'T02J3DPUE',
+    team: 'T02J3DPUE',
+    event: 'direct_message',
+    match: [ 'Hello', {index: 0}, {input: 'Hello'} ]
+  };
+
+  app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
+    console.log('Result ', message.watsonData);
+    app.getService('wchconversation').getWchConversationResponses(message.watsonData)
+    .then(result => console.log(result.conversationResp.searchResult.documents))
+    .catch(console.log);
+  });
+
+
+})
+.catch(console.log);
