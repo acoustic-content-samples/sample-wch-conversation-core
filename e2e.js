0bb7bce3da3aa2600f063bcaf5ca313eb4d55148
Sven Sterbling <sterbling@de.ibm.com>
2017-12-07 16:20:36 +0100

    Bugfixes

--- e2e.js
+++ e2e.js
@@ -74,13 +74,15 @@ resolveConfigAsync(architectConfig, __dirname)
   // app.getService('conversation').get('en').getWorkspace();
 
   app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
-    app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
-        console.log('Result ', message.watsonData);
-        console.log('Error ',  message.watsonError);
-        app.getService('wchconversation').getWchConversationResponses(message.watsonData)
-        .then(result => console.log(result.conversationResp.searchResult.documents))
-        .catch(console.log);
-    });
+      app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
+        app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
+            console.log('Result ', message.watsonData);
+            console.log('Error ',  message.watsonError);
+            app.getService('wchconversation').getWchConversationResponses(message.watsonData)
+            .then(result => console.log(result.conversationResp.searchResult.documents))
+            .catch(console.log);
+        });
+      });
   });
 
 
