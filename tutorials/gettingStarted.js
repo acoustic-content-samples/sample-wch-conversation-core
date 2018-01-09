882df70ada3e6cb44fd31bac151ca190fa07bd55
Sven Sterbling <sterbling@de.ibm.com>
2018-01-09 17:27:43 +0100

    Fixed linting errors.

--- tutorials/gettingStarted.js
+++ tutorials/gettingStarted.js
@@ -1,4 +1,4 @@
-  /**
+/**
  * Copyright 2017 IBM Corp. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,28 +27,28 @@ const appSettings = {
   "generalSettings": {
     "defaultLanguage": "en",
     "supportedLanguages": [
-     "de",
-     "en"
+      "de",
+      "en"
     ],
     "developermode": true,
     "confLvl": "0.7",
     "credentialsStore": {
-     "path": "./dch_vcap.json",
-     "encrypted": true,
-     "pathPrivKey": "C:\\Users\\SvenSterbling\\.ssh\\id_rsa"
+      "path": "./dch_vcap.json",
+      "encrypted": true,
+      "pathPrivKey": "C:\\Users\\SvenSterbling\\.ssh\\id_rsa"
     }
-   },
-   "wchService": {
-     "enabled": true
-   },
-   "conversationMiddleware": {
+  },
+  "wchService": {
+    "enabled": true
+  },
+  "conversationMiddleware": {
     "config": [
-     {
-      "workspaceId": "9db75ce4-a059-4f53-988d-de239eed10a0",
-      "locale": "en"
-     }
+      {
+        "workspaceId": "9db75ce4-a059-4f53-988d-de239eed10a0",
+        "locale": "en"
+      }
     ]
-   }
+  }
 };
 
 // This is the config - you can make changes to the settings either through
@@ -57,26 +57,23 @@ const architectConfig = require('../lib/conversation-core')(appSettings);
 const coreBase = dirname(require.resolve('../'));
 
 resolveConfigAsync(architectConfig, coreBase)
-.then(createAppAsync)
-.then(wchcore => {
-  // Access to your credentials
-  const env = wchcore.getService('env');
-  // Access to your Watson Conversation Service Instances
-  const conversation = wchcore.getService('conversation');
-  // Access to Watson Content Hub
-  const wchconversation =  wchcore.getService('wchconversation');
+  .then(createAppAsync)
+  .then(wchcore => {
+    // Access to your Watson Conversation Service Instances
+    const conversation = wchcore.getService('conversation');
+    // Access to Watson Content Hub
+    const wchconversation =  wchcore.getService('wchconversation');
 
-  conversation.get('en').sendMessage({message: 'Hello World', context: {}})
-  .then(watsonData => wchconversation.getWchConversationResponses(watsonData))
-  .then(({conversationResp, locationResp, followupResp}) => {
-    // The "general" answer is in the conversationResp
-    let text = conversationResp.searchResult.documents[0].document.elements.text.values[0]
-    console.log('The textual response from content hub is: ', text);
-    // If you have a location specific answer use the locationResp instead of the conversationResp
-    // If there are one time actions like asking for the username you have a followupResp as well as a conversationResp
-  })
-  .catch(err => {
-    console.log('Error ', err);
+    conversation.get('en').sendMessage({message: 'Hello World', context: {}})
+      .then(watsonData => wchconversation.getWchConversationResponses(watsonData))
+      .then(({conversationResp, locationResp, followupResp}) => {
+        // The "general" answer is in the conversationResp
+        let text = conversationResp.searchResult.documents[0].document.elements.text.values[0]
+        console.log('The textual response from content hub is: ', text);
+        // If you have a location specific answer use the locationResp instead of the conversationResp
+        // If there are one time actions like asking for the username you have a followupResp as well as a conversationResp
+      })
+      .catch(err => {
+        console.log('Error ', err);
+      });
   });
-
-});
