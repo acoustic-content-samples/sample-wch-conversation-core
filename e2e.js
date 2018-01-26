64b30451ebbc7883c6745145f85b74381d0408e4
Sven Sterbling <sterbling@de.ibm.com>
2018-01-26 13:15:43 +0100

    Fixed patsh

--- e2e.js
+++ e2e.js
@@ -24,67 +24,62 @@ const resolveConfigAsync = promisify(resolveConfig);
 const appSettings = require('./app_settings');
 const architectConfig = require('./lib/conversation-core')(appSettings);
 
-  process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: %o reason: %s', p, reason);
-  });
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: %o reason: %s', p, reason);
+});
 
 resolveConfigAsync(architectConfig, __dirname)
-.then(createAppAsync)
-.then(app => {
+  .then(createAppAsync)
+  .then(app => {
+    let initialContext = {
+      clientType: 'slack',
+      outputlang: '',
+      setoutputlang: true,
+      conversation_id: 'a2a7ea69-1452-4bb6-b1c2-a7506d639cc5',
+      wchid: '62cb7faa-0773-47e5-848e-4e660c4f3e5a',
+      counters: [],
+      gotfunky: false,
+      username: '',
+      chatbotpersona: 'mrwatson',
+      askedContactDetails: false,
+      nodename: 'Response First Welcome',
+      lastIntent: 'welcome'
+    }
 
-  let initialContext = {
-    clientType: 'slack',
-    outputlang: '',
-    setoutputlang: true,
-    conversation_id: 'a2a7ea69-1452-4bb6-b1c2-a7506d639cc5',
-    wchid: '62cb7faa-0773-47e5-848e-4e660c4f3e5a',
-    counters: [],
-    gotfunky: false,
-    username: '',
-    chatbotpersona: 'mrwatson',
-    askedContactDetails: false,
-    nodename: 'Response First Welcome',
-    lastIntent: 'welcome'
-  }
-
-  let bot =  {
-    type: 'slack',
-    botkit: {
-      storage: {
-        users: {
-          get: (userId, cb) => {cb(null, {context: initialContext})},
-          save: (user_data, cb) => {initialContext = user_data.context; cb();}
+    let bot =  {
+      type: 'slack',
+      botkit: {
+        storage: {
+          users: {
+            get: (userId, cb) => {cb(null, {context: initialContext})},
+            save: (user_data, cb) => {initialContext = user_data.context; cb();}
+          }
         }
       }
-    }
-  };
+    };
 
-  let message = {
-    type: 'message',
-    channel: 'D5BG9R0VD',
-    user: 'W4081DWRF',
-    text: 'Hallo',
-    ts: '1508942774.000657',
-    source_team: 'T02J3DPUE',
-    team: 'T02J3DPUE',
-    event: 'direct_message',
-    match: [ 'Hello', {index: 0}, {input: 'Hello'} ]
-  };
+    let message = {
+      type: 'message',
+      channel: 'D5BG9R0VD',
+      user: 'W4081DWRF',
+      text: 'Hallo',
+      ts: '1508942774.000657',
+      source_team: 'T02J3DPUE',
+      team: 'T02J3DPUE',
+      event: 'direct_message',
+      match: [ 'Hello', {index: 0}, {input: 'Hello'} ]
+    };
 
-  // app.getService('conversation').get('en').getWorkspace();
+    // app.getService('conversation').get('en').getWorkspace();
 
-  app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
+    app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
       app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
         app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
-            console.log('Result ', message.watsonData);
-            console.log('Error ',  message.watsonError);
-            app.getService('wchconversation').getWchConversationResponses(message.watsonData)
+          app.getService('wchconversation').getWchConversationResponses(message.watsonData)
             .then(result => console.log(result.conversationResp.searchResult.documents))
             .catch(console.log);
         });
       });
-  });
-
-
-})
-.catch(console.log);
+    });
+  })
+  .catch(console.log);
