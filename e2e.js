639ff5c7dee3056b3b7b51e20baad21f8e479dc3
Sven Sterbling <sterbling@de.ibm.com>
2018-06-26 16:49:47 +0200

    Added capability to define high level fallback to parent category and extend with special overwrites.

--- e2e.js
+++ e2e.js
@@ -33,7 +33,7 @@ resolveConfigAsync(architectConfig, __dirname)
   .then(app => {
     let initialContext = {
       clientType: 'slack',
-      outputlang: '',
+      outputlang: 'en',
       setoutputlang: true,
       conversation_id: 'a2a7ea69-1452-4bb6-b1c2-a7506d639cc5',
       wchid: '62cb7faa-0773-47e5-848e-4e660c4f3e5a',
@@ -41,9 +41,7 @@ resolveConfigAsync(architectConfig, __dirname)
       gotfunky: false,
       username: '',
       chatbotpersona: 'mrwatson',
-      askedContactDetails: false,
-      nodename: 'Response First Welcome',
-      lastIntent: 'welcome'
+      askedContactDetails: false
     }
 
     let bot =  {
@@ -58,11 +56,11 @@ resolveConfigAsync(architectConfig, __dirname)
       }
     };
 
-    let message = {
+    let hello = {
       type: 'message',
       channel: 'D5BG9R0VD',
       user: 'W4081DWRF',
-      text: 'Hallo',
+      text: 'Hello',
       ts: '1508942774.000657',
       source_team: 'T02J3DPUE',
       team: 'T02J3DPUE',
@@ -70,15 +68,44 @@ resolveConfigAsync(architectConfig, __dirname)
       match: [ 'Hello', {index: 0}, {input: 'Hello'} ]
     };
 
+    let message = {
+      type: 'message',
+      channel: 'D5BG9R0VD',
+      user: 'W4081DWRF',
+      text: 'Tell me something about MBUX in aclass',
+      ts: '1508942774.000657',
+      source_team: 'T02J3DPUE',
+      team: 'T02J3DPUE',
+      event: 'direct_message',
+      match: [ 'Tell me something about MBUX in aclass', {index: 0}, {input: 'Tell me something about MBUX in aclass'} ]
+    };
+
+
+    let messagewithcontext = {
+      type: 'message',
+      channel: 'D5BG9R0VD',
+      user: 'W4081DWRF',
+      text: 'Tell me something about MBUX',
+      ts: '1508942774.000657',
+      source_team: 'T02J3DPUE',
+      team: 'T02J3DPUE',
+      event: 'direct_message',
+      match: [ 'Tell me something about MBUX', {index: 0}, {input: 'Tell me something about MBUX'} ]
+    };
+
     // app.getService('conversation').get('en').getWorkspace();
 
-    app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
+    app.getService('conversationmiddleware').get('en').interpret(bot, hello, () => {
       app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
-        app.getService('conversationmiddleware').get('en').interpret(bot, message, () => {
-          app.getService('wchconversation').getWchConversationResponses(message.watsonData)
-            .then(result => console.log(result.conversationResp.searchResult.documents))
+
+        app.getService('conversationmiddleware').get('en').interpret(bot, messagewithcontext, () => {
+          app.getService('wchconversation').getWchConversationResponses(messagewithcontext.watsonData)
+            .then(result => {
+              console.log(JSON.stringify(result.conversationResp.searchResult.documents, null, 3))
+            })
             .catch(console.log);
         });
+
       });
     });
   })
